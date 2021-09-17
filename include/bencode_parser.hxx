4cd744bc476c9c12dcd31f58745af1d16d7e3d64
alyanser <staiz7071@gmail.com>
2021-09-18 00:53:48 +0500

    working state

--- include/bencode_parser.hxx
+++ include/bencode_parser.hxx
@@ -9,87 +9,112 @@
 #include <any>
 #include <optional>
 #include <vector>
+#include <iostream>
 
 namespace bencode {
 
+class bencode_error : std::exception {
+public:
+	using exception::exception;
+
+	template<typename T>
+	explicit bencode_error(T && error) : what_(error){}
+
+	const char * what [[nodiscard]] () const noexcept override {
+		return what_.data();
+	}
+private:
+	std::string_view what_;
+};
+
 namespace impl {
 
+using integer_result_type = std::optional<std::pair<int64_t,size_t>>;
+using label_result_type = std::optional<std::pair<std::string,size_t>>;
+using list_result_type = std::optional<std::pair<std::vector<std::any>,size_t>>;
+using dict_result_type = std::optional<std::pair<std::map<std::string,std::any>,size_t>>;
+
 template<typename T>
-std::optional<std::pair<int64_t,size_t>> extract_integer(T && content,const size_t content_length,size_t idx) noexcept {
+integer_result_type extract_integer(T && content,const size_t content_length,size_t idx){
 	assert(idx < content_length);
 
-	if(!std::isdigit(content[idx]) && content[idx] != '-' && content[idx] != 'i'){
+	if(content[idx] != 'i'){
 		return {};
 	}
- 
-	bool is_negative = false;
 
-	if(content[idx] == 'i'){
-		if(++idx == content_length){
-			return {};
-		}
+	constexpr std::string_view ending_not_found("Ending character ('e') not found for integral value");
 
-		is_negative |= content[idx] == '-';
-	}else if(content[idx] == '-'){
-		//! check if its possible according to the standard
-		return {};
+	if(++idx == content_length){
+		throw bencode_error(ending_not_found.data());
 	}
 
 	int64_t result = 0;
+	const bool negative = content[idx] == '-';
 
-	for(++idx;idx < content_length && content[idx] != 'e' && content[idx] != ':';++idx){
+	idx += negative;
+
+	for(;idx < content_length && content[idx] != 'e';++idx){
 		if(!std::isdigit(content[idx])){
-			return {};
+			throw bencode_error("Non-digits between 'i' and 'e'");
 		}
 
-		result += content[idx] - '0';
 		result *= 10;
+		result += content[idx] - '0';
 	}
 
-	if(idx == content_length || (content[idx] != ':' && content[idx] != 'e')){
-		return {};
+	if(idx == content_length){ // could not spot 'e'
+		throw bencode_error(ending_not_found.data());
 	}
 
-	return is_negative ? std::make_pair(-result,idx) : std::make_pair(result,idx);
+	assert(content[idx] == 'e');
+	return negative ? std::make_pair(-result,idx + 1) : std::make_pair(result,idx + 1);
 }
 
 template<typename T>
-std::optional<std::pair<std::string,size_t>> extract_label(T && content,const size_t content_length,size_t idx) noexcept {
+label_result_type extract_label(T && content,const size_t content_length,size_t idx){
 	assert(idx < content_length);
 
-	if(content[idx] == '-'){
+	if(!std::isdigit(content[idx])){
 		return {};
 	}
-	
-	//! cannot be const ig
-	if(const auto label_length_opt = extract_integer(std::forward<T>(content),idx,content_length)){
 
-		auto [label_length,forwarded_idx] = label_length_opt.value();
+	size_t label_length = 0;
 
-		assert(content[forwarded_idx] == ':');
-		idx = forwarded_idx + 1;
-
-		std::string result;
-
-		while(label_length--){
-			if(idx >= content_length){
-				return {};
-			}
-
-			result += content[idx++];
+	for(;idx < content_length && content[idx] != ':';idx++){
+		if(!std::isdigit(content[idx])){
+			throw bencode_error("Invalid character inside label length");
 		}
 
-		return std::make_pair(result,idx);
+		label_length *= 10;
+		label_length += static_cast<size_t>(content[idx] - '0');
 	}
 
-	return {};
+	if(idx >= content_length){
+		throw bencode_error("Label separator (:) was not found");
+	}
+
+	assert(content[idx] == ':');
+	++idx;
+
+	std::string result;
+	result.reserve(label_length);
+
+	while(label_length--){
+		if(idx >= content_length){
+			throw bencode_error("Label doesn't have enough characters as the associated length");
+		}
+
+		result += content[idx++];
+	}
+
+	return std::make_pair(result,idx);
 }
 
 template<typename T>
-std::optional<std::pair<std::map<std::string,std::any>,size_t>> extract_dict(T && content,size_t content_length,size_t idx) noexcept;
+dict_result_type extract_dict(T && content,size_t content_length,size_t idx);
 
 template<typename T>
-std::optional<std::pair<std::vector<std::any>,size_t>> extract_list(T && content,const size_t content_length,size_t idx) noexcept {
+list_result_type extract_list(T && content,const size_t content_length,size_t idx){
 	assert(idx < content_length);
 
 	if(content[idx] != 'l'){
@@ -99,36 +124,36 @@ std::optional<std::pair<std::vector<std::any>,size_t>> extract_list(T && content
 	std::vector<std::any> result;
 
 	for(++idx;idx < content_length;){
+		if(content[idx] == 'e'){
+			break;
+		}
+		
 		if(const auto integer_opt = extract_integer(std::forward<T>(content),content_length,idx)){
 			const auto [integer,forward_idx] = integer_opt.value();
-
-			result.push_back(integer);
+			result.emplace_back(integer);
 			idx = forward_idx;
 		}else if(const auto label_opt = extract_label(std::forward<T>(content),content_length,idx)){
-			const auto & [label,forward_idx] = label_opt.value();
-
-			result.push_back(std::move(label));
+			auto & [label,forward_idx] = label_opt.value();
+			result.emplace_back(std::move(label));
 			idx = forward_idx;
 		}else if(const auto list_opt = extract_list(std::forward<T>(content),content_length,idx)){
-			const auto & [list,forward_idx] = list_opt.value();
-
-			result.push_back(std::move(list));
+			auto & [list,forward_idx] = list_opt.value();
+			result.emplace_back(std::move(list));
 			idx = forward_idx;
 		}else if(const auto dict_opt = extract_dict(std::forward<T>(content),content_length,idx)){
-			const auto & [dict,forward_idx] = dict_opt.value();
-			
-			result.push_back(std::move(dict));
+			auto & [dict,forward_idx] = dict_opt.value();
+			result.emplace_back(std::move(dict));
 			idx = forward_idx;
 		}else{
 			return {};
 		}
 	}
 
-	return std::make_pair(result,idx);
+	return std::make_pair(result,idx + 1);
 }
 
 template<typename T>
-std::optional<std::pair<std::map<std::string,std::any>,size_t>> extract_dict(T && content,size_t content_length,size_t idx) noexcept {
+dict_result_type extract_dict(T && content,size_t content_length,size_t idx){
 	assert(idx < content_length);
 
 	if(content[idx] != 'd'){
@@ -138,6 +163,10 @@ std::optional<std::pair<std::map<std::string,std::any>,size_t>> extract_dict(T &
 	std::map<std::string,std::any> result;
 
 	for(++idx;idx < content_length;){
+		if(content[idx] == 'e'){
+			break;
+		}
+		
 		const auto key_opt = extract_label(std::forward<T>(content),content_length,idx);
 
 		if(!key_opt.has_value()){
@@ -147,25 +176,21 @@ std::optional<std::pair<std::map<std::string,std::any>,size_t>> extract_dict(T &
 		const auto & [key,key_forward_idx] = key_opt.value();
 
 		idx = key_forward_idx;
-		
+
 		if(const auto integer_opt = extract_integer(std::forward<T>(content),content_length,idx)){
 			const auto [integer,forward_idx] = integer_opt.value();
-
 			result.emplace(key,integer);
 			idx = forward_idx;
 		}else if(const auto label_opt = extract_label(std::forward<T>(content),content_length,idx)){
-			const auto & [label,forward_idx] = label_opt.value();
-
+			auto & [label,forward_idx] = label_opt.value();
 			result.emplace(key,std::move(label));
 			idx = forward_idx;
 		}else if(const auto list_opt = extract_list(std::forward<T>(content),content_length,idx)){
-			const auto & [list,forward_idx] = list_opt.value();
-
+			auto & [list,forward_idx] = list_opt.value();
 			result.emplace(key,std::move(list));
 			idx = forward_idx;
 		}else if(const auto dict_opt = extract_dict(std::forward<T>(content),content_length,idx)){
-			const auto & [dict,forward_idx] = dict_opt.value();
-			
+			auto & [dict,forward_idx] = dict_opt.value();
 			result.emplace(key,std::move(dict));
 			idx = forward_idx;
 		}else{
@@ -173,24 +198,29 @@ std::optional<std::pair<std::map<std::string,std::any>,size_t>> extract_dict(T &
 		}
 	}
 
-	return std::make_pair(result,idx);
+	return std::make_pair(result,idx + 1);
 }
 
 } // namespace impl
 
 template<typename T>
-inline auto parse_from_content(T && content){
+inline std::map<std::string,std::any> parse_from_content(T && content){
 	const auto content_length = std::size(content);
-	return impl::extract_dict(std::forward<T>(content),content_length,0);
+
+	if(const auto dict_opt = impl::extract_dict(std::forward<T>(content),content_length,0)){
+		const auto & [dict,forward_idx] = dict_opt.value();
+		return dict;
+	}
+
+	return {};
 }
 
 template<typename T>
-inline auto parse_from_file(T && file_path){
+auto parse_from_file(T && file_path){
 	std::ifstream in_fstream(std::forward<T>(file_path));
 
 	if(!in_fstream.is_open()){
-		//todo find appropriate thing
-		throw std::invalid_argument("File could not be opened");
+		throw bencode_error("File doesn't exist or could not be opened for writing");
 	}
 
 	std::string content;
--- tests/main.cxx
+++ tests/main.cxx
@@ -1,6 +1,13 @@
 #include "bencode_parser.hxx"
 #include <iostream>
 
-int main(){
-	bencode::parse_from_file("sample.torrent");
+int main() try {
+	const auto result = bencode::parse_from_file("sample.torrent");
+
+	for(const auto & [key,value] : result){
+		std::cout << key << '\n';
+		// std::cout << std::any_cast<int32_t>(value) << '\n';
+	}
+}catch(const std::exception & exception){
+	std::cout << exception.what() << '\n';
 }
