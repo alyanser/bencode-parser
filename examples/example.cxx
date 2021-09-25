937455f59daa91c2435cc3171b4c23393824b3e3
alyanser <staiz7071@gmail.com>
2021-09-26 01:27:23 +0500

    metadata only

--- examples/example.cxx
+++ examples/example.cxx
@@ -5,11 +5,11 @@ int main(){
 	const auto result = bencode::parse_file("../examples/sample-input/big-buck-bunny.torrent");
 	const auto metadata = bencode::extract_metadata(result);
 
-	// possibly containing non-standard compliant keys and values
-	std::cout << bencode::convert_to_string(result) << '\n';
-
 	// only evaluate dictionary standard dictionary keys
 	std::cout << bencode::convert_to_string(metadata) << '\n';
 
-	std::cout << metadata.raw_info_dict << '\n';
+	// possibly containing non-standard compliant keys and values
+	// std::cout << bencode::convert_to_string(result) << '\n';
+
+	// std::cout << metadata.raw_info_dict << '\n';
 }
