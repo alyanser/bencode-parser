7aee10f5aa194e665a678265e3ba95e3b4287c63
alyanser <staiz7071@gmail.com>
2021-10-10 21:59:16 +0500

    minor fixes

--- examples/example.cxx
+++ examples/example.cxx
@@ -2,11 +2,15 @@
 #include <iostream>
 
 int main(){
-	const auto result = bencode::parse_file("../examples/sample-input/big-buck-bunny.torrent");
+	const auto result = bencode::parse_file("../examples/sample-input/test.torrent");
 	const auto metadata = bencode::extract_metadata(result);
 
 	// only evaluate dictionary standard dictionary keys
 	std::cout << bencode::convert_to_string(metadata) << '\n';
+
+	for(const auto & [file_path,size] : metadata.file_info){
+		std::cout << file_path << ' ' << size << '\n';
+	}
 
 	// possibly containing non-standard compliant keys and values
 	// std::cout << bencode::convert_to_string(result) << '\n';
