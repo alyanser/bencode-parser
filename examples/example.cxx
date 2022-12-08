9e849f77cb2be105bb02fa93f5bda85e37198a7c
Aly Anser <alyystaz@gmail.com>
2022-12-08 21:04:52 +0500

    tabbified + changed exception member to view

--- examples/example.cxx
+++ examples/example.cxx
@@ -2,18 +2,18 @@
 #include <iostream>
 
 int main(){
-         const auto result = bencode::parse_file("../examples/sample-input/sintel.torrent");
-         const auto metadata = bencode::extract_metadata(result);
-         
-         // only evaluate dictionary standard dictionary keys
-         std::cout << bencode::convert_to_string(metadata) << '\n';
+	const auto result = bencode::parse_file("../examples/sample-input/sintel.torrent");
+	const auto metadata = bencode::extract_metadata(result);
+	
+	// only evaluate dictionary standard dictionary keys
+	std::cout << bencode::convert_to_string(metadata) << '\n';
 
-         for(const auto & [file_path,size] : metadata.file_info){
-                  std::cout << file_path << ' ' << size << '\n';
-         }
+	for(const auto & [file_path,size] : metadata.file_info){
+		std::cout << file_path << ' ' << size << '\n';
+	}
 
-         // possibly containing non-standard compliant keys and values
-         // std::cout << bencode::convert_to_string(result) << '\n';
+	// possibly containing non-standard compliant keys and values
+	// std::cout << bencode::convert_to_string(result) << '\n';
 
-         // std::cout << metadata.raw_info_dict << '\n';
+	// std::cout << metadata.raw_info_dict << '\n';
 }
